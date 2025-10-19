83347185b99e419293238d430a43de76423a4a53
Seth Nickell <snickell@gmail.com>
2025-10-18 20:51:01 -1000

    Play with some type ideas

--- src/nodes/HiveDoc.ts
+++ src/nodes/HiveDoc.ts
@@ -2,10 +2,10 @@ import type { Edge, Node} from '@xyflow/react'
 import type { JSNode } from './js/model.ts'
 
 // FIXME: eventually we should disallow generic `Node` types here
-type HiveNode = JSNode | Node
+type HiveDocValidNodeTypes = JSNode | Node
 
 export type HiveDoc = {
   version: 1
-  nodes: HiveNode[]
+  nodes: HiveDocValidNodeTypes[]
   edges: Edge[]
 }
--- src/nodes/js/index.ts
+++ src/nodes/js/index.ts
@@ -1,3 +1,4 @@
+import { type Runnable } from '../Runnable.ts'
 import {
   JS_NODE_TYPE,
   DEFAULT_JS_SNIPPET,
@@ -6,9 +7,13 @@ import {
 
 import { JsNodeView } from './view.tsx'
 
-export class JS {
+export class JS implements Runnable {
   static NODE_TYPE = JS_NODE_TYPE
   static DEFAULT_SNIPPET = DEFAULT_JS_SNIPPET
   static createNode = createJsNode
   static View = JsNodeView
+
+  run(): void {
+    throw new Error('Method not yet implemented.')
+  }
 }
